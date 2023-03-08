68ee4e30d03a5fe25d213539438d281e8a9a1743
Shahar Tsuk <shahartzuk23@gmail.com>
2023-03-08 19:26:36 +0200

    Start Project
    fix datalayer

---
+++ News-Flashes project server/News.Model/Article.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Model
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "LinkImage")]
+        public string Title { get; set; }
+
+        [Required, Display(Name = "Description")]
+        public string LinkImage { get; set; }
+
+        [Required, Display(Name = "Title")]
+        public string Description { get; set; }
+
+        [Required, Display(Name = "WebLink")]
+        public string WebLink { get; set; }
+
+        [Required, Display(Name = "Subject")]
+        public Subject subject { get; set; }
+    }
+}
