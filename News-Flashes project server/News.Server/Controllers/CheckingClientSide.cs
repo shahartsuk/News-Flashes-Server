d4ae635295bf537c79cd429d46476bb8da030091
Shahar Tsuk <shahartzuk23@gmail.com>
2023-03-12 12:58:54 +0200

    enable cors

---
+++ News-Flashes project server/News.Server/Controllers/CheckingClientSide.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using News.Entities;
+
+namespace News.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Subjects")]
+    public class CheckingClientSide
+    {
+        [HttpGet("GetSubjects")]
+        public JsonResult GetAllCitiesList()
+        {
+            return new JsonResult(MainManager.Instance.dataLayer.Subjects.ToList());
+        }
+    }
+}
